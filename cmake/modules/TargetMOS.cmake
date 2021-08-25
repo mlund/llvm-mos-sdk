70556ef62dda8fe4b6ac5f969651eb6bfb7313a5
John Byrd <jbyrd@giganticsoftware.com>
2021-08-25 07:35:03 -0700

    Don't test compile on llvm-mos

--- cmake/modules/TargetMOS.cmake
+++ cmake/modules/TargetMOS.cmake
@@ -1,9 +1,15 @@
 # TargetMOS.cmake
 # CMake pass for targeting MOS.
 
+# Since this is the MOS build, and we've found our own compiler, don't run 
+# the command line tests on CMake startup
+set(CMAKE_C_COMPILER_WORKS 1)
+set(CMAKE_CXX_COMPILER_WORKS 1)
+include(Platform/llvm-mos)
 include(BootstrapCompiler)
 include(DetectCompiler)
 detect_compiler(_result)
 if (NOT _result)
-    message(FATAL_ERROR "need to find own compiler")
+    message(FATAL_ERROR "failed to discover LLVM-MOS compiler")
 endif()
+include(Platform/llvm-mos)
