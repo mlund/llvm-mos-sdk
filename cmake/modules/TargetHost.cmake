3696f81daa5f920ca698aa10423f20911a4bc125
John Byrd <jbyrd@giganticsoftware.com>
2021-08-31 19:42:09 -0700

    Moved simulator to utils directory (it's a utility? it's a target?)

--- cmake/modules/TargetHost.cmake
+++ cmake/modules/TargetHost.cmake
@@ -2,11 +2,11 @@
 # Build programs that must be run on this host
 
 include(ExternalProject)
-add_subdirectory(platform/sim/sim)
 
-# Run the superbuild-style rebuild of this project, with the different compiler
-# suite
-message(INFO "Project dir is ${PROJECT_SOURCE_DIR}")
+add_subdirectory(utils/sim/sim)
+
+# Run the superbuild-style rebuild of this project, with the compiler
+# suite for local builds
 ExternalProject_Add(llvm_mos_sdk_mos
     SOURCE_DIR ${PROJECT_SOURCE_DIR}
     CMAKE_ARGS -DLLVM_MOS_COMPILE_TARGET_MOS=On ${CMAKE_ARGS}
